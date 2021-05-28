49e6669ab997c76b9212e48c1e7e09adcbfe7830
Dorasima-98 <cjk1516@gmail.com>
2021-05-29 01:24:39 +0900

    KU_FS

---
+++ KU_FS/cmake-build-debug/CMakeFiles/KU_FS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KU_FS.dir/main.c.obj"
+  "KU_FS.exe"
+  "KU_FS.exe.manifest"
+  "KU_FS.pdb"
+  "libKU_FS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KU_FS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
